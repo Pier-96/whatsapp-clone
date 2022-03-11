69b0c1c22237d84c2db64f9caf2c8709014bb8fd
pieroolivares <piero-olivares@hotmail.com>
2022-03-11 13:32:59 +0100

    added sidebar + css styling

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVfjqV4HY21OKzCr0N9dX8K6RE_HpJ0mM",
+  authDomain: "whatsappclone-9b350.firebaseapp.com",
+  projectId: "whatsappclone-9b350",
+  storageBucket: "whatsappclone-9b350.appspot.com",
+  messagingSenderId: "470359497140",
+  appId: "1:470359497140:web:08b5fc0d96431207d53ae1",
+  measurementId: "G-BTVCM5W6MT"
+};
